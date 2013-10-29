4315fbb0c448cf6d2736559d9ad5b7162e2e14c0
Thomas v d O <ohet@gmx.de>
2013-10-29 13:41:00 -0700

    smaller changes to tile html

--- js/tile.js
+++ js/tile.js
@@ -4,27 +4,46 @@
 
 docReady( function() {
 
-  var container = document.querySelector('.masonry');
-  var msnry = new Masonry( container, {
-    columnWidth: 50
-  });
-
-$('#addTileButton').bind('click', function() {
-  var testClass = document.createElement('div');
-testClass.id ='test';
-testClass.className = "item";
-var rand = Math.random()*100;
-console.log(rand);
-testClass.style.width=rand +"px";
-var rand = Math.random()*100;
-testClass.style.height=rand +"px";
+	var container = document.querySelector('.masonry');
+	var msnry = new Masonry( container, {
+		columnWidth: 2
+	});
 
 
-container.appendChild( testClass );
-msnry.appended( testClass );
-msnry.layout();
 
-});
+	msnry.bindResize()
+
+	$('#addTileButton').bind('click', function() {
+		var testClass = document.createElement('div');
+		testClass.id ='test';	
+		testClass.className = "item";
+		
+		var testClassItemContent = document.createElement('div');
+		testClassItemContent.className = "item-content";
+		
+		testClass.appendChild(testClassItemContent);
+		var rand = Math.random()*300;
+		if (rand<=100) {
+			rand = 100;
+		} 
+		console.log(rand);
+		testClass.style.width=rand +"px";
+		testClassItemContent.style.width=rand +"px";
+		
+		var rand = Math.random()*300;
+		if (rand<=100) {
+			rand = 100;
+		} 
+		
+		testClass.style.height=rand +"px";
+		testClassItemContent.style.height=rand +"px";
+
+
+		container.appendChild( testClass );
+		msnry.appended( testClass );
+		msnry.layout();
+
+	});
 
 
 
