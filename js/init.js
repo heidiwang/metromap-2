f161030a6df3b5a321b813311eff8c229507f8c9
Thomas v d O <ohet@gmx.de>
2013-12-03 15:42:11 -0800

    changed init.js and added Request

--- js/init.js
+++ js/init.js
@@ -2,6 +2,19 @@
 Map: Initialize variables and stage/canvas
 **************************/
 
+var serverURL = "ilws19.stanford.edu:8089"
+var searchQuery = "tennis";
+var dateString = "W06142013";
+var url = serverURL + "/" + searchQuery + "/" + dateString;
+
+var data;
+$.getJSON( url, function( data ) {
+  console.log(data);
+});
+
+
+
+
 var nodes = data.nodes;
 var lines = data.lines;
 
